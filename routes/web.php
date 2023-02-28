880f79231de61760725de87b353b3aa0be8af6af
DESKTOP-Q0UOC6Q\ADMIN <ngochanbuinguyen@gmail.com>
2023-02-28 10:45:08 +0700

    regist-page

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/login-page', function () {
+    return view('login-page');
+});
+Route::get('/home-page', function () {
+    return view('home-page');
+});
+Route::get('/regist-page', function () {
+    return view('regist-page');
+});
